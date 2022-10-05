2fbd36a43a9a9748bda3b8f78da511bbe3855097
Insu Hong <hinsu93@gmail.com>
2022-10-05 16:50:09 +0900

    minutes & hours

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import { hourSelector, minuteState } from "./atom";
 
 function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(Number(event.currentTarget.value));
+  };
+
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(Number(event.currentTarget.value));
+  };
+
   return (
     <div>
-      <h1>Hi</h1>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input
+        value={hours}
+        onChange={onHoursChange}
+        type="number"
+        placeholder="Hours"
+      />
     </div>
   );
 }
